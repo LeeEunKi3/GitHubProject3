e6bd895f91b56e4f3568f8be30896bece2901c71
user <user@DESKTOP-COJ9858>
2022-06-28 16:52:35 +0900

    한글테스트commit

--- GroupGitProject4/src/com/sist/kim/MainClass.java
+++ GroupGitProject4/src/com/sist/kim/MainClass.java
@@ -3,7 +3,7 @@ package com.sist.kim;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("¾È³çÇÏ¼¼¿ä~");
+		System.out.println("ì•ˆë…•í•˜ì„¸ìš”~~~~");
 	}
 
 }
